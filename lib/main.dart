dc371b4eee9dce13857109c2322e3f80e1ae9f7b
nishtha-dev <nishthasaraswat15@gmail.com>
2023-12-18 13:51:20 +0530

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
-import 'package:financial_goal_detail/src/controller/provider/data_provider.dart';
+import 'package:financial_goal_detail/firebase_options.dart';
+import 'package:financial_goal_detail/src/controller/bloc/goal_details_bloc.dart';
+
+import 'package:financial_goal_detail/src/controller/goal_repository/goal_repository_impl.dart';
+
 import 'package:financial_goal_detail/src/core/theme/app_theme.dart';
 import 'package:financial_goal_detail/src/view/screens/bottom_tab_bar.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -18,8 +26,9 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       builder: (context, child) {
         ScreenUtil.init(context, designSize: const Size(360, 801));
-        return ChangeNotifierProvider(
-          create: (context) => DataProvider(),
+        return BlocProvider(
+          create: (context) => GoalDetailsBloc(
+              goalDataServiceRepository: GoalDataServiceRepositoryImpl()),
           child: MaterialApp(
             debugShowCheckedModeBanner: false,
             title: 'Financial Goal Detail Page',
