ca51468379eb3bcb015ae220d453315ef86e6c71
nishtha-dev <nishthasaraswat15@gmail.com>
2023-12-18 13:50:56 +0530

    add theme data

--- lib/src/core/theme/app_theme.dart
+++ lib/src/core/theme/app_theme.dart
@@ -1,4 +1,4 @@
-import 'package:financial_goal_detail/src/core/constants/colors.dart';
+import 'package:financial_goal_detail/src/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
@@ -11,23 +11,7 @@ class AppTheme {
       scaffoldBackgroundColor: AppColors.white,
       splashColor: AppColors.transparent,
       progressIndicatorTheme:
-          const ProgressIndicatorThemeData(color: AppColors.payneGreyColor),
-      dividerColor: AppColors.dividerColor,
+          const ProgressIndicatorThemeData(color: AppColors.green),
     );
   }
-
-  // AppBarTheme get appBarTheme {
-  //   return const AppBarTheme(
-  //     iconTheme: IconThemeData(
-  //       color: AppColors.black,
-  //     ),
-  //     elevation: 0,
-  //     toolbarHeight: 64,
-  //     backgroundColor: AppColors.payneGreyColor,
-  //     systemOverlayStyle: SystemUiOverlayStyle(
-  //       statusBarIconBrightness: Brightness.dark,
-  //       statusBarBrightness: Brightness.light,
-  //     ),
-  //   );
-  // }
 }
